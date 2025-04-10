05b845acc9b86b81ce1b9d3e4545733bc53ddf41
Keyaru <keyaruga.reese@proton.me>
2025-04-10 15:07:05 +0530

    Update: Install.sh
    Run commands with sudo.

--- Install.sh
+++ Install.sh
@@ -32,11 +32,11 @@ echo -e "${GREEN}Starting the installation of Grub Themes${NC}"
 sleep 2
 FIRST_SCRIPT_URL="https://raw.githubusercontent.com/Keyaruga-Reese/grub-theme-installer/refs/heads/main/GrubThemeDownloader.sh"  # Replace with the actual URL
 curl -s -O $FIRST_SCRIPT_URL
-chmod +x GrubThemeDownloader.sh
-./GrubThemeDownloader.sh
+sudo chmod +x GrubThemeDownloader.sh
+sudo ./GrubThemeDownloader.sh
 
 # Delete the first script after execution
-rm GrubThemeDownloader.sh
+sudo rm GrubThemeDownloader.sh
 
 # Delay for 2 seconds
 sleep 2
@@ -45,11 +45,11 @@ sleep 2
 echo -e "${GREEN}Creating scrpit to change GRUB Theme randomly${NC}"
 SECOND_SCRIPT_URL="https://raw.githubusercontent.com/Keyaruga-Reese/grub-theme-installer/refs/heads/main/GrubThemeChanger.sh"  # Replace with the actual URL
 curl -s -O $SECOND_SCRIPT_URL
-mv GrubThemeChanger.sh /boot/grub/themes/GrubThemeChanger.sh
-chmod +x /boot/grub/themes/GrubThemeChanger.sh
+sudo mv GrubThemeChanger.sh /boot/grub/themes/GrubThemeChanger.sh
+sudo chmod +x /boot/grub/themes/GrubThemeChanger.sh
 
 # Run the second script after moving it.
-bash /boot/grub/themes/GrubThemeChanger.sh
+sudo bash /boot/grub/themes/GrubThemeChanger.sh
 
 # Delay for 2 seconds
 sleep 2
@@ -57,7 +57,7 @@ sleep 2
 # Step 3: Create a systemd service for the second script.
 echo -e "${GREEN}Creating the systemd service to change theme after every reboot${NC}"
 SYSTEMD_SERVICE_FILE="/etc/systemd/system/GrubThemeChanger.service"
-echo "[Unit]
+sudo echo "[Unit]
 Description=Set random GRUB theme at boot
 
 [Service]
@@ -73,6 +73,6 @@ sleep 2
 
 # Enable the systemd service to run at boot
 echo -e "${GREEN}Enabling the systemd service${NC}"
-systemctl enable GrubThemeChanger.service
+sudo systemctl enable GrubThemeChanger.service
 
 echo -e "${GREEN}Setup complete. The systemd service has been created and enabled.${NC}"
